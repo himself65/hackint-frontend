0bbc4115c2debeb28b00ea55086c93cbce76d272
himself65 <himself65@outlook.com>
2019-07-10 01:23:54 +0800

    feat: add hover animation on member cards

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -10,48 +10,52 @@
         v-for="member in members"
         class="xs12 lg5"
       >
-        <v-card
-          class="mx-auto member"
-          :color="member.color"
-          dark
-          max-width="400"
-        >
-          <v-card-title>
-            <v-list-tile class="grow">
-              <v-list-tile-avatar color="grey darken-3">
-                <v-img
-                  class="elevation-3"
-                  :src="require(`../assets/member/${member.avatar}`)"
-                ></v-img>
-              </v-list-tile-avatar>
+        <v-hover>
+          <v-card
+            slot-scope="{ hover }"
+            :class="`elevation-${hover ? 12 : 2}`"
+            class="mx-auto member"
+            :color="member.color"
+            dark
+            max-width="400"
+          >
+            <v-card-title>
+              <v-list-tile class="grow">
+                <v-list-tile-avatar color="grey darken-3">
+                  <v-img
+                    class="elevation-3"
+                    :src="require(`../assets/member/${member.avatar}`)"
+                  ></v-img>
+                </v-list-tile-avatar>
 
-              <v-list-tile-content>
-                <v-list-tile-title class="font-weight-bold">{{ member.name }}</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-card-title>
+                <v-list-tile-content>
+                  <v-list-tile-title class="font-weight-bold">{{ member.name }}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-card-title>
 
-          <v-card-text class="headline font-weight-light">
-            {{ member.description || '这个人很强，什么也不想写' }}
-          </v-card-text>
+            <v-card-text class="headline font-weight-light">
+              {{ member.description || '这个人很强，什么也不想写' }}
+            </v-card-text>
 
-          <v-card-actions>
-            <v-btn
-              flat
-              color="grey lighten-5"
-              :href="`https://github.com/${member.github}`"
-              target="_blank">
-              Github
-            </v-btn>
-            <v-btn
-              flat
-              color="light-blue lighten-1"
-              :href="`https://zhihu.com/people/${member.zhihu}`"
-              target="_blank">
-              知乎
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+            <v-card-actions>
+              <v-btn
+                flat
+                color="grey lighten-5"
+                :href="`https://github.com/${member.github}`"
+                target="_blank">
+                Github
+              </v-btn>
+              <v-btn
+                flat
+                color="light-blue lighten-1"
+                :href="`https://zhihu.com/people/${member.zhihu}`"
+                target="_blank">
+                知乎
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-hover>
       </v-flex>
     </v-layout>
   </v-container>
